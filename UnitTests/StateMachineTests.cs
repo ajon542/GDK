11b3b9cda7bc4c139db593e404a08bfa9a810c87
Andrew Jones <ajon542@gmail.com>
2015-11-24 16:57:47 -0800

    Added unit test project

---
+++ UnitTests/StateMachineTests.cs
@@ -0,0 +1,15 @@
+﻿
+namespace UnitTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class StateMachineTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
