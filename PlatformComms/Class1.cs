01682975fd8cd3474c65705fe807d4daf7f03399
Andrew Jones <ajon542@gmail.com>
2015-11-24 19:04:07 -0800

    Added description and examples of platform comms

--- PlatformComms/Class1.cs
+++ PlatformComms/Class1.cs
@@ -6,6 +6,30 @@ using System.Threading.Tasks;
 
 namespace PlatformComms
 {
+    // Communication between the GameServer and the Platform.
+    // There are going to be two types of communication between the game server and the platform.
+    // The first is when the game server makes a request and the platform responds. For example,
+    // the game server may request random numbers or tell the platform that it wishes to start a
+    // new wager game.
+    // The second is when the platform notifies the game server of some event such as a suspend
+    // or when credits have been updated.
+
+    // Register game server with platform
+    // Game configuration -> paytable, button layout
+    // Wager start
+    // Wager end
+    // Base game start
+    // Base game end
+    // Bonus start (could include free games since this is really just a bonus)
+    // Bonus end
+    // Get random numbers
+    // Suspend / Resume
+    // Insert demo credits
+    // Credits updated
+    // Pay win
+    // Enter / Exit History
+
+
     public class Class1
     {
     }
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Game Development Kit is a framework for developing slot machine style games.
 
 ## Notes
-Implemented basic Game and Slot state machine. It is fairly easy to add states with the setup I currently have, however, I'm not conviced using strings for the states is a great idea once the state machine gets larger.
+Implemented basic Game and Slot state machine. It is fairly easy to add states with the setup I currently have, however, I'm not convinced using strings for the states is a great idea once the state machine gets larger.
 
 Implemented basic paytable structure and investigating the evaluation methods. This is going to take a few more days.
 
