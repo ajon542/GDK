c97149dac06e33be8543cd02c40d34f6b32eb043
Andrew Jones <ajon542@gmail.com>
2015-11-25 16:08:01 -0800

    more comments on platform comms

--- PlatformComms/Class1.cs
+++ PlatformComms/Class1.cs
@@ -14,6 +14,17 @@ namespace PlatformComms
     // The second is when the platform notifies the game server of some event such as a suspend
     // or when credits have been updated.
 
+    // There are also different scenarios when we may or may not want to communicate with the platform.
+    // For example during normal game play, we are always communicating with the platform. Normal game play
+    // occurs between Wager start and Wager end.
+    // Once a game is completed, it is possible that we wish to recover the game as in history or if a power
+    // hit occurs. There will be no comms with the platform and all information will come from the Replay log.
+    // If a powerhit occurs before the Wager end is specified, then the game will communicate with the platform
+    // as per normal, except for when it draws random numbers. Why?
+    // Why can't platform also store the random numbers. That way it will handle the recovery of those too??
+    // No because we want to control infinite free games in our game stream. That way, we handle the random numbers
+    // and we can still call functions
+
     // Register game server with platform
     // Game configuration -> paytable, button layout
     // Wager start
