1ac41b5e7f45c1e3c23d52e6b8197166a0c627d9
Andrew Jones <andrew.jones@tapslots.com>
2017-01-08 10:59:06 -0800

    Added scatter evaluation tests

--- GDK/Assets/Components/MathEngine/UnitTests/Editor/ScatterEvaluatorTests.cs
+++ GDK/Assets/Components/MathEngine/UnitTests/Editor/ScatterEvaluatorTests.cs
@@ -16,10 +16,30 @@ public class ScatterEvaluatorTests
 	[TestFixtureSetUp]
 	public void Init ()
 	{
-		DummyPaytableGenerator generator = new DummyPaytableGenerator ();
-		paytable = generator.Generate ();
-
+		paytable = new Paytable ();
 		scatterEvaluator = new ScatterEvaluator ();
+
+		paytable.ReelGroup = new ReelGroup ();
+		paytable.ScatterComboGroup = new PayComboGroup ();
+
+		ReelStrip reel1 = new ReelStrip ();
+		reel1.AddSymbol (new Symbol (0, "AA"));
+		reel1.AddSymbol (new Symbol (1, "BB"));
+		reel1.AddSymbol (new Symbol (2, "CC"));
+
+		ReelStrip reel2 = new ReelStrip ();
+		reel2.AddSymbol (new Symbol (0, "AA"));
+		reel2.AddSymbol (new Symbol (1, "BB"));
+		reel2.AddSymbol (new Symbol (2, "CC"));
+
+		ReelStrip reel3 = new ReelStrip ();
+		reel3.AddSymbol (new Symbol (0, "AA"));
+		reel3.AddSymbol (new Symbol (1, "BB"));
+		reel3.AddSymbol (new Symbol (2, "CC"));
+
+		paytable.ReelGroup.AddReel (reel1);
+		paytable.ReelGroup.AddReel (reel2);
+		paytable.ReelGroup.AddReel (reel3);
 	}
 
 	[Test]
@@ -33,35 +53,41 @@ public class ScatterEvaluatorTests
 	[Test]
 	public void Evaluation_SlotResult1 ()
 	{
-		// Reel Window
-		// AA AA AA
-		// BB BB BB
-		// CC CC CC
-
-		ReelGroup reels = new ReelGroup ();
-
-		ReelStrip reel1 = new ReelStrip ();
-		reel1.AddSymbol (new Symbol (0, "AA"));
-		reel1.AddSymbol (new Symbol (1, "BB"));
-		reel1.AddSymbol (new Symbol (2, "CC"));
-
-		ReelStrip reel2 = new ReelStrip ();
-		reel2.AddSymbol (new Symbol (3, "DD"));
-		reel2.AddSymbol (new Symbol (4, "EE"));
-		reel2.AddSymbol (new Symbol (5, "FF"));
-
-		ReelStrip reel3 = new ReelStrip ();
-		reel3.AddSymbol (new Symbol (6, "GG"));
-		reel3.AddSymbol (new Symbol (7, "HH"));
-		reel3.AddSymbol (new Symbol (8, "II"));
-
-		reels.AddReel (reel1);
-		reels.AddReel (reel2);
-		reels.AddReel (reel3);
-
-		paytable.ReelGroup = reels;
 		paytable.ScatterComboGroup.Combos.Clear ();
 		paytable.ScatterComboGroup.AddPayCombo (new PayCombo(new Symbol(0, "AA"), 3, 1000));
+
+		rng = new DummyRng (new List<int> { 0, 0, 0 });
+		SlotResults results = scatterEvaluator.Evaluate (paytable, rng);
+		Assert.AreEqual (1, results.Results.Count);
+	}
+
+	[Test]
+	public void Evaluation_SlotResult2 ()
+	{
+		paytable.ScatterComboGroup.Combos.Clear ();
+		paytable.ScatterComboGroup.AddPayCombo (new PayCombo(new Symbol(1, "BB"), 3, 1000));
+
+		rng = new DummyRng (new List<int> { 0, 0, 0 });
+		SlotResults results = scatterEvaluator.Evaluate (paytable, rng);
+		Assert.AreEqual (1, results.Results.Count);
+	}
+
+	[Test]
+	public void Evaluation_SlotResult3 ()
+	{
+		paytable.ScatterComboGroup.Combos.Clear ();
+		paytable.ScatterComboGroup.AddPayCombo (new PayCombo(new Symbol(1, "BB"), 2, 1000));
+
+		rng = new DummyRng (new List<int> { 0, 0, 0 });
+		SlotResults results = scatterEvaluator.Evaluate (paytable, rng);
+		Assert.AreEqual (1, results.Results.Count);
+	}
+
+	[Test]
+	public void Evaluation_SlotResult4 ()
+	{
+		paytable.ScatterComboGroup.Combos.Clear ();
+		paytable.ScatterComboGroup.AddPayCombo (new PayCombo(new Symbol(1, "BB"), 4, 1000));
 
 		rng = new DummyRng (new List<int> { 0, 0, 0 });
 		SlotResults results = scatterEvaluator.Evaluate (paytable, rng);
