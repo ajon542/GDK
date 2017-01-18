5e56b014fa4302c2279a899a2233e5ccf6c5408a
Andrew Jones <andrew.jones@tapslots.com>
2017-01-17 16:12:00 -0800

    Updated some unit tests

--- GDK/Assets/Components/MathEngine/UnitTests/Editor/ScatterEvaluatorTests.cs
+++ GDK/Assets/Components/MathEngine/UnitTests/Editor/ScatterEvaluatorTests.cs
@@ -79,4 +79,56 @@ public class ScatterEvaluatorTests
 		SlotResults results = scatterEvaluator.Evaluate (paytable, rng);
 		Assert.AreEqual (0, results.Results.Count);
 	}
+
+	[Test]
+	public void Evaluation_SlotResult5 ()
+	{
+		paytable.ScatterComboGroup.Combos.Clear ();
+		paytable.ScatterComboGroup.AddPayCombo (new PayCombo(new Symbol(0, "AA"), 3, 1000));
+		paytable.ScatterComboGroup.AddPayCombo (new PayCombo(new Symbol(1, "BB"), 3, 500));
+		paytable.ScatterComboGroup.AddPayCombo (new PayCombo(new Symbol(2, "CC"), 3, 10));
+
+		// TODO: This will start coming together when we figure out how all the results accumulate.
+		rng = new DummyRng (new List<int> { 0, 0, 0 });
+		SlotResults results = scatterEvaluator.Evaluate (paytable, rng);
+		Assert.AreEqual (3, results.Results.Count);
+		Assert.AreEqual (1000, results.Results [0].TotalValue);
+		Assert.AreEqual (500, results.Results [1].TotalValue);
+		Assert.AreEqual (10, results.Results [2].TotalValue);
+	}
+
+	[Test]
+	public void Evaluation_SlotResult6 ()
+	{
+		paytable.ScatterComboGroup.Combos.Clear ();
+		paytable.ScatterComboGroup.AddPayCombo (new PayCombo(new Symbol(0, "AA"), 1, 10));
+		paytable.ScatterComboGroup.AddPayCombo (new PayCombo(new Symbol(0, "AA"), 2, 100));
+
+		// TODO: This is a bug in the scatter evaluation.
+		rng = new DummyRng (new List<int> { 0, 0, 0 });
+		SlotResults results = scatterEvaluator.Evaluate (paytable, rng);
+		Assert.AreEqual (1, results.Results.Count);
+		Assert.AreEqual (100, results.Results [0].TotalValue);
+	}
+
+	[Test]
+	public void Evaluation_SlotResult7 ()
+	{
+		List<Symbol> combo = new List<Symbol> () {
+			new Symbol (0, "AA"),
+			new Symbol (1, "BB"),
+			new Symbol (2, "CC")
+		};
+
+		paytable.ScatterComboGroup.Combos.Clear ();
+		paytable.ScatterComboGroup.AddPayCombo (new PayCombo(combo, 150));
+
+		// TODO: This is a bug in the scatter evaluation.
+		rng = new DummyRng (new List<int> { 0, 0, 0 });
+		SlotResults results = scatterEvaluator.Evaluate (paytable, rng);
+		Assert.AreEqual (3, results.Results.Count);
+		Assert.AreEqual (150, results.Results [0].TotalValue);
+		Assert.AreEqual (150, results.Results [1].TotalValue);
+		Assert.AreEqual (150, results.Results [2].TotalValue);
+	}
 }
