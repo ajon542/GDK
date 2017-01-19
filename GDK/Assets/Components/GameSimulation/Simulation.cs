d7078ed2d16a5d20bee5ee02762e49c7236d903c
Andrew Jones <andrew.jones@tapslots.com>
2017-01-19 01:00:05 -0800

    Added basic simulation scripts

---
+++ GDK/Assets/Components/GameSimulation/Simulation.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+using GDK.MathEngine;
+using GDK.MathEngine.Evaluators;
+
+namespace GDK.GameSimulation
+{
+	public class Simulation : MonoBehaviour
+	{
+		private PaytableBuilder builder = new SimulationPaytableBuilder ();
+		private Paytable paytable = new Paytable ();
+		private IEvaluator evaluator = new PaylineEvaluator ();
+		private IRng rng = new Rng ();
+		private Thread simulationThread;
+
+		private void Start ()
+		{
+			paytable.ReelGroup = builder.BuildReelGroup ();
+			paytable.PaylineGroup = builder.BuildPaylineGroup ();
+			paytable.PayComboGroup = builder.BuildPayComboGroup ();
+
+			simulationThread = new Thread (Simulate);
+			simulationThread.Start ();
+		}
+
+		private int bet = 1;
+		private int totalBet;
+		private int totalWin;
+
+		private int displayCount = 100000;
+		private int numberOfSimulations = 1000000;
+
+		private void Simulate()
+		{
+			while (numberOfSimulations-- > 0)
+			{
+				totalBet += bet;
+
+				SlotResults results = evaluator.Evaluate (paytable, rng);
+
+				foreach (var result in results.Results)
+				{
+					totalWin += result.PayCombo.PayAmount;
+				}
+
+				displayCount--;
+				if (displayCount == 0)
+				{
+					Debug.Log (string.Format ("TotalWin={0}, TotalBet={1}, RTP={2}", 
+						totalWin, 
+						totalBet, 
+						(float)totalWin / (float)totalBet));
+					displayCount = 100000;
+				}
+			}
+		}
+	}
+}
---
+++ GDK/Assets/Components/GameSimulation/SimulationPaytableBuilder.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using GDK.MathEngine;
+
+namespace GDK.GameSimulation
+{
+	public class SimulationPaytableBuilder : PaytableBuilder
+	{
+		public override ReelGroup BuildReelGroup ()
+		{
+			ReelGroup reels = new ReelGroup ();
+
+			ReelStrip reel1 = new ReelStrip ();
+			reel1.AddSymbol (new Symbol (0, "AA"));
+			reel1.AddSymbol (new Symbol (1, "BB"));
+
+			ReelStrip reel2 = new ReelStrip ();
+			reel2.AddSymbol (new Symbol (0, "AA"));
+			reel2.AddSymbol (new Symbol (1, "BB"));
+
+			ReelStrip reel3 = new ReelStrip ();
+			reel3.AddSymbol (new Symbol (0, "AA"));
+			reel3.AddSymbol (new Symbol (1, "BB"));
+
+			reels.AddReel (reel1);
+			reels.AddReel (reel2);
+			reels.AddReel (reel3);
+
+			return reels;
+		}
+
+		public override PaylineGroup BuildPaylineGroup ()
+		{
+			PaylineGroup paylines = new PaylineGroup ();
+
+			Payline payline1 = new Payline ();
+			payline1.AddPaylineCoord (new PaylineCoord { ReelIndex = 0, Offset = 0 });
+			payline1.AddPaylineCoord (new PaylineCoord { ReelIndex = 1, Offset = 0 });
+			payline1.AddPaylineCoord (new PaylineCoord { ReelIndex = 2, Offset = 0 });
+
+			paylines.AddPayline (payline1);
+
+			return paylines;
+		}
+
+		public override PayComboGroup BuildPayComboGroup ()
+		{
+			PayComboGroup payCombos = new PayComboGroup ();
+
+			payCombos.AddPayCombo (new PayCombo (new Symbol (0, "AA"), 3, 5));
+			payCombos.AddPayCombo (new PayCombo (new Symbol (1, "BB"), 3, 2));
+
+			return payCombos;
+		}
+	}
+}
