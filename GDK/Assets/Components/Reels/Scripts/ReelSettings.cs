245b6be6884e8ad22296a7e598347b32d2e48e85
Andrew Jones <andrew.jones@tapslots.com>
2017-02-01 21:43:32 -0800

    Added new file for animation curve extensions and reel settings

---
+++ GDK/Assets/Components/Reels/Scripts/ReelSettings.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GDK.Reels
+{
+	public class ReelSettings : MonoBehaviour
+	{
+		[SerializeField] private int visibleSymbols;
+		[SerializeField] private float spinTime;
+		[SerializeField] private float symbolSpacing;
+
+		public int VisibleSymbols { get { return visibleSymbols; } }
+
+		public float SpinTime { get { return spinTime; } }
+
+		public float SymbolSpacing { get { return symbolSpacing; } }
+	}
+}
---
+++ GDK/Assets/Components/Utilities/AnimationCurveUtilities.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace GDK.Utilities
+{
+	public static class AnimationCurveExtensions
+	{
+		/// <summary>
+		/// Align the game objects along the y-axis evenly between start and end values.
+		/// </summary>
+		/// <param name="gameObjects">The game objects to align.</param>
+		/// <param name="valueStart">The start value (inclusive).</param>
+		/// <param name="valueEnd">The end value (inclusive).</param>
+		public static void AlignVerticalCenter (List<GameObject> gameObjects, float valueStart, float valueEnd)
+		{
+			AnimationCurve layoutCurve = AnimationCurve.Linear (0, valueStart, 1, valueEnd);
+			float firstPos = 1.0f / (gameObjects.Count - 1);
+
+			for (int i = 0; i < gameObjects.Count; ++i)
+			{
+				float y = layoutCurve.Evaluate (i * firstPos);
+				UpdatePosition (gameObjects [i].transform, 0, y, 0);
+			}
+		}
+
+		private static void UpdatePosition (Transform t, float x, float y, float z)
+		{
+			t.position = new Vector3 (t.position.x + x, t.position.y + y, t.position.z + z);
+		}
+	}
+}
