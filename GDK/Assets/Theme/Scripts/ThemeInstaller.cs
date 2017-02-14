c4519a5dbb1986320adae1c704cfe60e921c2151
Andrew Jones <ajon542@gmail.com>
2017-02-13 21:02:27 -0800

    Added another reel and my own background.

--- GDK/Assets/Theme/Scripts/ThemeInstaller.cs
+++ GDK/Assets/Theme/Scripts/ThemeInstaller.cs
@@ -51,6 +51,10 @@ namespace Theme
             {
                 Symbols = new List<Symbol> { new Symbol(0, "AA"), new Symbol(1, "BB"), new Symbol(2, "CC"), new Symbol(3, "DD"), new Symbol(3, "EE"), new Symbol(4, "FF"), new Symbol(5, "GG"), new Symbol(6, "HH") }
             });
+            paytable.ReelGroup.AddReel(new ReelStrip
+            {
+                Symbols = new List<Symbol> { new Symbol(0, "AA"), new Symbol(1, "BB"), new Symbol(2, "CC"), new Symbol(3, "DD"), new Symbol(3, "EE"), new Symbol(4, "FF"), new Symbol(5, "GG"), new Symbol(6, "HH") }
+            });
 
             //paytable.ReelGroup = builder.BuildReelGroup ();
             //paytable.PaylineGroup = builder.BuildPaylineGroup ();
