93dc73e0fa0913f52f4a538cfef7e2572a8810f3
Andrew Jones <ajon542@gmail.com>
2017-02-26 14:11:06 -0800

    Remove symbol group

--- GDK/Assets/Components/MathEngine/Paytable.cs
+++ GDK/Assets/Components/MathEngine/Paytable.cs
@@ -3,18 +3,17 @@ using System.Collections.Generic;
 
 namespace GDK.MathEngine
 {
-	/// <summary>
-	/// Represents a simple paytable for a slot game.
-	/// </summary>
-	[Serializable]
-	public class Paytable
-	{
-        public SymbolGroup SymbolGroup { get; set; }
-		public ReelGroup ReelGroup { get; set; }
-		public PaylineGroup PaylineGroup { get; set; }
-		public PayComboGroup PayComboGroup { get; set; }
-		public PayComboGroup ScatterComboGroup { get; set; }
-		public PaytableTriggerGroup PaytableTriggerGroup { get; set; }
-		public PickTableGroup PickTableGroup { get; set; }
-	}
+    /// <summary>
+    /// Represents a simple paytable for a slot game.
+    /// </summary>
+    [Serializable]
+    public class Paytable
+    {
+        public ReelGroup ReelGroup { get; set; }
+        public PaylineGroup PaylineGroup { get; set; }
+        public PayComboGroup PayComboGroup { get; set; }
+        public PayComboGroup ScatterComboGroup { get; set; }
+        public PaytableTriggerGroup PaytableTriggerGroup { get; set; }
+        public PickTableGroup PickTableGroup { get; set; }
+    }
 }
